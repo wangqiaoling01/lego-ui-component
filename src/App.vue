746f5df373e99e7413f48bdd85cc8406d82ed529
wangqiaoling <1113584801@qq.com>
2023-10-18 08:44:24 +0800

    feat: 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <home />
+</template>
+<script lang="ts">
+import Home from './views/Home.vue'
+export default {
+    components: {
+        Home
+    }
+}
+</script>
+<style scoped>
+</style>
